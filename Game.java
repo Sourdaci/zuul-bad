02a632737bcd3f6e9bc3c0e12a1ed45b0f822cd3
Sourdaci <sourdaci@gmail.com>
2015-04-08 12:56:20 +0200

    Zuul cambiado por Greg, mapa nuevo y mas largo

--- Game.java
+++ Game.java
@@ -34,23 +34,40 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room recibidor, pasillo, dormServicio, salaEstar, biblioteca, cocina, comedor, 
+            servEmpleados, pasilloTrasero, dormPadres, servPadres, dormGreg, servGreg;
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        recibidor = new Room("Entrada de la Casa");
+        pasillo = new Room("Pasillo Delantero");
+        dormServicio = new Room("Habitaciones de los Empleados");
+        salaEstar = new Room("Salon");
+        biblioteca = new Room("Biblioteca. Muchos libros");
+        cocina = new Room("Cocina. La cena huele bien...");
+        comedor = new Room("Comedor. Estan preparando la mesa para cenar");
+        servEmpleados = new Room("'Aliviaderos' de los Empleados. Huele a muerto. Y mucho");
+        pasilloTrasero = new Room("Pasillo Trasero");
+        dormPadres = new Room("Dormitorio de los padres de Greg");
+        servPadres = new Room("Servicio de los padres de Greg");
+        dormGreg = new Room("Dormitorio de Greg");
+        servGreg = new Room("Servicio de Greg");
         
         // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        recibidor.setExits(pasillo, null, null, null);
+        pasillo.setExits(comedor, salaEstar, recibidor, dormServicio);
+        dormServicio.setExits(cocina, pasillo, null, null);
+        salaEstar.setExits(biblioteca, null, null, pasillo);
+        biblioteca.setExits(null, null, salaEstar, null);
+        cocina.setExits(servEmpleados, comedor, dormServicio, null);
+        comedor.setExits(pasilloTrasero, null, pasillo, cocina);
+        servEmpleados.setExits(null, null, cocina, null);
+        pasilloTrasero.setExits(dormGreg, dormPadres, comedor, null);
+        dormPadres.setExits(null, null, servPadres, pasilloTrasero);
+        servPadres.setExits(dormPadres, null, null, null);
+        dormGreg.setExits(null, null, pasilloTrasero, servGreg);
+        servGreg.setExits(null, dormGreg, null, null);
 
-        currentRoom = outside;  // start game outside
+        currentRoom = recibidor;  // start game outside
     }
 
     /**
@@ -77,12 +94,12 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type 'help' if you need help.");
+        System.out.println("Estas en casa de tu amigo Greg. Quieres... no, NECESITAS ir al servicio");
+        System.out.println("No te ha dicho donde esta, solo que no entres en otro servicio de la casa");
+        System.out.println("Escribe 'help' si andas perdido");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
+        System.out.println("Estas en: " + currentRoom.getDescription());
+        System.out.print("Direcciones disponibles: ");
         if(currentRoom.northExit != null) {
             System.out.print("north ");
         }
@@ -108,7 +125,7 @@ public class Game
         boolean wantToQuit = false;
 
         if(command.isUnknown()) {
-            System.out.println("I don't know what you mean...");
+            System.out.println("Te quedas en el sitio aguantando");
             return false;
         }
 
@@ -135,10 +152,10 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+        System.out.println("Necesitas encontrar un servicio en condiciones");
+        System.out.println("en casa de tu colega Greg urgentemente.");
         System.out.println();
-        System.out.println("Your command words are:");
+        System.out.println("Tus acciones disponibles son:");
         System.out.println("   go quit help");
     }
 
@@ -150,7 +167,7 @@ public class Game
     {
         if(!command.hasSecondWord()) {
             // if there is no second word, we don't know where to go...
-            System.out.println("Go where?");
+            System.out.println("Si no indicas donde, no te vas a mover");
             return;
         }
 
@@ -172,12 +189,12 @@ public class Game
         }
 
         if (nextRoom == null) {
-            System.out.println("There is no door!");
+            System.out.println("No atraviesas paredes ni abres ventanas, listo...");
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
+            System.out.println("Estas en: " + currentRoom.getDescription());
+            System.out.print("Direcciones Disponibles: ");
             if(currentRoom.northExit != null) {
                 System.out.print("north ");
             }
@@ -202,7 +219,7 @@ public class Game
     private boolean quit(Command command) 
     {
         if(command.hasSecondWord()) {
-            System.out.println("Quit what?");
+            System.out.println("No te rindas, aun puedes llegar...");
             return false;
         }
         else {
