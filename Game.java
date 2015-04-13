c7fa25cfdee856769f5e5e1f3403b35d5405ddb1
Sourdaci <sourdaci@gmail.com>
2015-04-13 08:53:10 +0200

    Comando look agregado para mostrar la informacion de la habitacion actual

--- Game.java
+++ Game.java
@@ -140,6 +140,8 @@ public class Game
         }
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
+        }else if (commandWord.equals("look")){
+            printLocationInfo();
         }
 
         return wantToQuit;
